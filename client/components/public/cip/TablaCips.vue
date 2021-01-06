610c07c495a30cb675a1f181b73d41bfb2e5b9de
unknown <yonar.ramos@systelec.com>
2021-01-06 08:31:05 -0300

    tabla cip

--- client/components/public/cip/TablaCips.vue
+++ client/components/public/cip/TablaCips.vue
@@ -12,16 +12,22 @@
               label="Seleccione un equipo"
             ></v-select>
           </v-flex>
-          <v-flex xs8 text-xs-right>
-            <v-btn large icon color="blue-grey" dark @click="exportToCSV()">
+        <v-flex xs2 sm2>
+           
+          </v-flex>
+
+          <v-flex xs6 text-xs-right>
+            <v-btn large icon color="blue" dark @click="csvExport(csvData)">
               <v-icon>get_app</v-icon>
             </v-btn>
           </v-flex>
         </v-layout>
       </v-card-title>
+
       <v-data-table
         :headers="headers"
         :items="cips"
+        :loading="loading"
         style="width: 100%"
         class="elevation-0"
         no-data-text="No hay datos registrados en el sistema"
@@ -32,50 +38,27 @@
       >
         <v-progress-linear slot="progress" color="blue" indeterminate></v-progress-linear>
         <template slot="items" slot-scope="props">
-          
           <tr
             @click="props.expanded = !props.expanded"
             class="text-xs-left"
             style="cursor: pointer;"
           >
             <td>
-              <strong> {{props.item.circuito}} </strong>
+              <strong class="blue-grey--text">{{ props.item.Description }}</strong>
             </td>
-            <td><!-- {{ props.item.inicio | formatearFecha }} --> {{props.item.fecha}}</td>
-            <td>{{props.item.tiempo}}</td>
-            <td>
-              <strong>{{props.item.litros_drenaje}}</strong>
-            </td>
-            <td><v-text-field v-model="props.item.targe_litros" type="number" class="body-1"  flat solo hide-details placeholder="Indicar litros"></v-text-field></td>
-            <!-- <td>targe/litros = targe/litros  - litros/drenaje el resultado</td> -->
-            <td><v-text-field flat solo hide-details readonly class="body-1 red--text" style="color: #000 !important;" :value="props.item.targe_litros ? props.item.desvio = props.item.targe_litros - props.item.litros_drenaje : 'Esperando valor de target'" ></v-text-field></td>
-            <td>{{ props.item.tipo_limpieza }}</td>
-            <td>{{ props.item.c_final}}</td>
+            <td>{{ props.item.Description | circuito }}</td>
+            <td> {{props.item.EventStamp | fecha}}</td>
+            <td><strong>{{ props.item.duracion /60 | redondear }}</strong> min</td>
+            <td>{{ props.item.Value }}</td> 
+            <td><v-text-field v-model="props.item.target" type="number" class="body-1"  flat solo hide-details placeholder="Indicar litros"></v-text-field></td>
+            <td><v-text-field flat solo hide-details readonly class="body-1 red--text" style="color: #000 !important;" :value="props.item.target ? props.item.desvio = props.item.target - props.item.Value : '...'" ></v-text-field></td>
+            <td>{{ props.item.Description | FilterModo }}</td>
+            <td>{{ props.item.Description | filterAprobado }}</td>
+             
           </tr>
         </template>
-        <template slot="expand" slot-scope="props">
-          <v-card flat>
-            <v-card-text>
-              <v-timeline align-top dense>
-                <template v-for="(item, i) in props.item.operaciones">
-                  <v-timeline-item :key="i" color="pink" small>
-                    <v-layout pt-3>
-                      <v-flex xs3>
-                        <strong>{{item.fecha | formatearFecha}}</strong>
-                      </v-flex>
-                      <v-flex>
-                        <strong>{{item.descripcion}}</strong>
-                        <div class="caption">Estado: {{item.estado}}</div>
-                        <div class="caption">Modo: {{item.modo}}</div>
-                      </v-flex>
-                    </v-layout>
-                  </v-timeline-item>
-                </template>
-              </v-timeline>
-            </v-card-text>
-          </v-card>
-        </template>
       </v-data-table>
+  
     </v-card>
   </div>
 </template>
@@ -88,17 +71,34 @@ import axios from '@/plugins/axios'
 import mixins from '@/mixins'
 import exportFromJSON from 'export-from-json'
 import _ from 'lodash'
+import  json from "@/static/DATOS_CIP.json";
 export default {
+
   data: () => ({
+    cips: [],
+    cip:{
+            "EventStamp": "",
+            "TagName": "",
+            "Description": "",
+            "Area": "",
+            "Value": "",
+            "target":null,
+            "desvio":null
+        },
+    datos: json,
+    drenaje:null,
+    prueba:'',
     headers: [
-      { text: 'N° de Circuito', value: 'Nombre' },
-      { text: 'Fecha', value: 'Inicio' },
-      { text: 'Tiempo', value: 'Inicio' },
-      { text: 'Litros/Drenaje', value: 'Fin' },
-      { text: 'Targe Litros', value: 'Duracion' },
-      { text: 'Desvio', value: 'desvio' },
-      { text: 'Tipo de Limpieza', value: 'vol_sani' },
-      { text: 'Cond. Final', value: 'vol_env' },
+      { text: 'Descripción', value:'duracion'},
+      { text: 'N° de circuito', value: 'description' },
+      { text: 'Fecha', value: 'vol_env'},
+      { text: 'Tiempo', value: 'vol_sani'},
+      { text: 'Litro/Drenaje', value: 'inicio' },
+      { text: 'Target Litros', value: 'fin' },
+      { text: 'Desvio', value:'Duracion'},
+      { text: 'Tipo Limpieza', value:'Duracion'},
+      { text: 'Cond. Final', value:'Duracion'},
+      
     ],
     itemsCIP: [
       {
@@ -139,64 +139,90 @@ export default {
       }
     ],
     loading: false,
-    cips: [{
-        circuito:"1",
-        fecha:"11-23-2002",
-        tiempo:"11",
-        litros_drenaje:10,
-        targe_litros:null,
-        desvio:null,
-        tipo_limpieza:"Profunda",
-        c_final:"Optima"
-    },
-    {
-        circuito:"2",
-        fecha:"11-23-2023",
-        tiempo:"17",
-        litros_drenaje:25,
-        targe_litros:null,
-        desvio:null,
-        tipo_limpieza:"Superficial",
-        c_final:"Regular"
-    },
-    {
-        circuito:"3",
-        fecha:"11-30-2023",
-        tiempo:"15",
-        litros_drenaje:60,
-        targe_litros:null,
-        desvio:null,
-        tipo_limpieza:"Superficial",
-        c_final:"Regular"
-    }],
-    equipos: [],
-    cipSeleccionado: ['CIP11']
+    runtime:[],
+    equipos:[],
+    cipSeleccionado: ['CIP11'],
+    numeroObjeto:[''],
   }),
 
   mixins: [mixins],
 
   computed: {
-    ...mapGetters(['aplicarFiltro', 'desde', 'hasta'])
+    ...mapGetters(['aplicarFiltro', 'desde', 'hasta']),
+    BusquedaPorNombre: function(){
+    //  return this.cips.filter((item) => item.descripcion.match(this.nombredeobjeto))
+    },
+    csvData() {
+      return this.BusquedaPorNombre.map(item => ({
+        ...item,
+        duracion: item.duracion,
+        operaciones: item.operaciones[0].descripcion,
+        Fecha: item.operaciones[0].fecha,
+        Estados: item.operaciones[0].estado,
+        
+        Modo: item.operaciones[0].modo
+      }))
+    
+      
+    }
   },
 
   watch: {
     aplicarFiltro() {
-      this.getAlarmas()
+      this.getCips()
     }
   },
+  filters:{
+    filterAprobado: function(value){
+     let fin = value.includes('Fin')
+     let abor = value.includes('Abortado')
+     
+     if(fin){
+       return 'OK'
+     }
+     if(abor){
+       return 'Abortado'
+     }
 
-  methods: {
-    calcularDesvio(){
-      let desvio=0
-     desvio = this.cipsItem.litros_drenaje - this.cipsItem.targe_litros
-     return desvio
     },
-    ...mapMutations(['SET_APLICAR_FILTRO']),
-    async getAlarmas() {
-      this.loading = true
+    FilterModo: function(value){
+      let modo = value.includes('Modo')
+      if(modo){
+        return 'LAVADO'
+      }
+    },
+    fecha: function(value){
+     let  hasta = 
+      moment(value)
+        .format('YYYY-MM-DD HH:mm:ss')
+   
+    return hasta
+    },
+    circuito: function(value){
+      let exp = /( [0-9]\S+)/
+      let res = exp.exec(value)
+      if(res){
+        if(res[0] == " 0Modo:"){
+          return ''
+        }
+        return res[0]
+      }
+      return ''
+    }
+  },
+created(){
+    this.getCips()
+},
+  methods: {
 
-      await axios
-        .get('cips', {
+    ...mapMutations(['SET_APLICAR_FILTRO']),
+    async getCips() {
+      let arr = [];
+      this.loading = true
+      alert(this.desde,this.hasta,)
+
+        await axios
+        .get('cips1', {
           params: {
             desde: this.desde,
             hasta: this.hasta,
@@ -204,49 +230,47 @@ export default {
           }
         })
         .then(response => {
-          this.cips = response.data
-          this.loading = false
+          arr = response.data.cips;
+          this.runtime = response.data.runtime;
         })
-    },
-    exportToCSV() {
-      const fileName = 'reportes_cips'
-      const exportType = 'csv'
-      const data = []
-      this.cips.map(alarma => {
-        data.push({
-          Descripcion: alarma.descripcion,
-          Inicio: alarma.inicio,
-          Fin: alarma.fin,
-          Duracion_min: mathjs.round(alarma.duracion / 60, 2),
-          Vol_env: alarma.vol_env,
-          Vol_sani: alarma.vol_sani
-        })
+        this.cips=[];
 
-        if (alarma.operaciones.length > 0) {
-          alarma.operaciones.map(item => {
-            data.push({
-              Descripcion: '    ' + item.descripcion,
-              Inicio: item.fecha,
-              Fin: '',
-              Duracion: '',
-              Vol_env: item.modo,
-              Vol_sani: item.estado
-            })
-          })
+        for (let i = 0;i<arr.length;i++) {
+          let index =  this.runtime.findIndex(element => element.DateTime.toString().slice(0,10) == arr[i].EventStamp.toString().slice(0,10));
+          if(index != -1){
+            arr[i].Value = this.runtime[index].Value;
+          }else{
+            arr[i].Value = 0
+          }
+          this.cip = arr[i]
+          this.cips.push(this.cip);
         }
-      })
-      exportFromJSON({ data, fileName, exportType })
+      this.loading = false
+    },
+    csvExport(arrData) {
+      let csvContent = "data:text/csv;charset=utf-8,";
+      csvContent += [
+        Object.keys(arrData[0]).join(";"),
+        ...arrData.map(item => Object.values(item).join(";"))
+      ]
+        .join("\n")
+        .replace(/(^\[)|(\]$)/gm, "");
+
+      const data = encodeURI(csvContent);
+      const link = document.createElement("a");
+      link.setAttribute("href", data);
+      link.setAttribute("download", "export.csv");
+      link.click();
     }
   }
 }
 </script>
 
-
 <style>
-input[type=number]::-webkit-inner-spin-button, 
-input[type=number]::-webkit-outer-spin-button { 
-  -webkit-appearance: none; 
-  margin: 0; 
-}
-input[type=number] { -moz-appearance:textfield; }
-</style>  
+  input[type=number]::-webkit-inner-spin-button, 
+  input[type=number]::-webkit-outer-spin-button { 
+    -webkit-appearance: none; 
+    margin: 0; 
+  }
+  input[type=number] { -moz-appearance:textfield; }
+</style> 
